ae967d9ddc7135cf7ba9d6abbd0768d72f6f0987
fayismahmood <fmthottathil@gmail.com>
2024-02-21 09:26:51 +0530

    packageed

--- src/lib/context/ModelCmp.svelte
+++ src/lib/context/ModelCmp.svelte
@@ -7,18 +7,20 @@
   import { receive, send } from "$lib/animate/sendRecive.js";
  
 //let {Models}=$props<{Models:ModelStore}>()
-
+let dialogs=(quick.models || []).filter((e) => e.type == "dialog") as  DialogModel[]
+let drawers=(quick.models || []).filter((e) => e.type == "drawer") as DrawerModel[]
+let toasts=(quick.models || []).filter((e) => e.type == "toast") as ToastModel[]
 </script>
 
-{#each (quick.models || []).filter((e) => e.type == "dialog") as e }
-  <Dialog {...e.props as DialogModel["props"]} />
+{#each dialogs as e }
+  <Dialog {...e.props} />
 {/each}
-{#each (quick.models || []).filter((e) => e.type == "drawer") as e}
+{#each drawers as e}
   <Drawer {...e.props as DrawerModel["props"]} />
 {/each}
 
 <div class="fixed bottom-5 right-5 grid gap-3">
-  {#each (quick.models || []).filter((e) => e.type == "toast") as e,i(e.key)}
+  {#each toasts as e,i(e.key)}
     <div animate:flip={{ duration: 200 }}>
       <Toast  {...e.props as ToastModel["props"]} />
     </div>
