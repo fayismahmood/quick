60f8c3eac78c5877150a71f64c66c10399f9beec
fayismahmood <fmthottathil@gmail.com>
2024-02-21 09:28:58 +0530

    asdf

--- src/lib/context/ModelCmp.svelte
+++ src/lib/context/ModelCmp.svelte
@@ -3,26 +3,37 @@
   import Drawer from "$lib/modals/drawer.svelte";
   import Toast from "$lib/modals/toast.svelte";
   import { flip } from "svelte/animate";
-  import {quick, type DialogModel, type DrawerModel, type ToastModel } from "./models.svelte.js";
+  import {
+    quick,
+    type DialogModel,
+    type DrawerModel,
+    type ToastModel,
+  } from "./models.svelte.js";
   import { receive, send } from "$lib/animate/sendRecive.js";
- 
-//let {Models}=$props<{Models:ModelStore}>()
-let dialogs=(quick.models || []).filter((e) => e.type == "dialog") as  DialogModel[]
-let drawers=(quick.models || []).filter((e) => e.type == "drawer") as DrawerModel[]
-let toasts=(quick.models || []).filter((e) => e.type == "toast") as ToastModel[]
+
+  //let {Models}=$props<{Models:ModelStore}>()
+  let dialogs = (quick.models || []).filter(
+    (e) => e.type == "dialog"
+  ) as DialogModel[];
+  let drawers = (quick.models || []).filter(
+    (e) => e.type == "drawer"
+  ) as DrawerModel[];
+  let toasts = (quick.models || []).filter(
+    (e) => e.type == "toast"
+  ) as ToastModel[];
 </script>
 
-{#each dialogs as e }
+{#each dialogs as e}
   <Dialog {...e.props} />
 {/each}
 {#each drawers as e}
-  <Drawer {...e.props as DrawerModel["props"]} />
+  <Drawer {...e.props} />
 {/each}
 
 <div class="fixed bottom-5 right-5 grid gap-3">
-  {#each toasts as e,i(e.key)}
+  {#each toasts as e, i (e.key)}
     <div animate:flip={{ duration: 200 }}>
-      <Toast  {...e.props as ToastModel["props"]} />
+      <Toast {...e.props} />
     </div>
   {/each}
 </div>
